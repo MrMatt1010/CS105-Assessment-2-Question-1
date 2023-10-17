f84e010ba5900769eb45fe8a556aa794476eff01
Matt T <mrsymtec@gmail.com>
2023-10-17 17:29:38 +1300

    Add project files.

---
+++ CS105 Assessment 2 Question 1/CS105 Assessment 2 Question 1.cpp
@@ -0,0 +1,13 @@
+// CS105 Assessment 2 Question 1
+// Student Name: Matt Taylor
+// This program will allow up to 5 people to track their average steps as per the brief
+
+#include <iostream>
+
+
+
+int main()
+{
+    
+}
+
