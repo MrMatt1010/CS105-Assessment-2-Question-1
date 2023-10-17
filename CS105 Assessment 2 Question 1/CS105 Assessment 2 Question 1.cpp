ac5f82347afeac0f1de2649b31771a0113eb503f
Matt T <mrsymtec@gmail.com>
2023-10-18 09:41:18 +1300

    Corrected an error with cal of the average walking and running dist.

--- CS105 Assessment 2 Question 1/CS105 Assessment 2 Question 1.cpp
+++ CS105 Assessment 2 Question 1/CS105 Assessment 2 Question 1.cpp
@@ -6,20 +6,23 @@
 #include <string>
 #include <vector>
 
-//Base class
+// Base class
 class healthActivity {
 private:
     std::string name;
     int steps;
     double walking;
     double running;
+    double combinedDistance;
 
 public:
     // Default constructor
-    healthActivity() : steps(0), walking(0.0), running(0.0) {}
+    healthActivity() : steps(0), walking(0.0), running(0.0), combinedDistance(0.0) {}
 
     healthActivity(int s, double w, double r, const std::string& n)
-        : steps(s), walking(w), running(r), name(n) {}
+        : steps(s), walking(w), running(r), name(n) {
+        combinedDistance = w + r;
+    }
 
     // Getter methods
     std::string getName() const {
@@ -31,11 +34,15 @@ public:
     }
 
     double getWalking() const {
-        return walking * 0.001;
+        return walking;
     }
 
     double getRunning() const {
-        return running * 0.001;
+        return running;
+    }
+
+    double getCombinedDistance() const {
+        return combinedDistance;
     }
 
     // Setter methods
@@ -55,6 +62,21 @@ public:
         running = r;
     }
 };
+
+// Function to calculate and display the average combined distance
+double calculateAverageCombinedDistance(const std::vector<healthActivity>& people) {
+    double totalCombinedDistance = 0.0;
+
+    for (const healthActivity& person : people) {
+        totalCombinedDistance += person.getCombinedDistance();
+    }
+
+    if (people.size() > 0) {
+        return totalCombinedDistance / people.size();
+    }
+
+    return 0.0; // Use this to avoid division by zero
+}
 
 int main() {
     std::vector<healthActivity> people; // Vector of healthActivity objects 
@@ -89,10 +111,17 @@ int main() {
         std::cout << "Steps: " << person.getSteps() << std::endl;
         std::cout << "Walking distance (km): " << person.getWalking() << std::endl;
         std::cout << "Running distance (km): " << person.getRunning() << std::endl;
+        std::cout << "Combined distance (km): " << person.getCombinedDistance() << std::endl;
         std::cout << "-------------------------\n";
     }
 
+    double averageCombinedDistance = calculateAverageCombinedDistance(people);
+    std::cout << "Average Combined Distance (km): " << averageCombinedDistance << std::endl;
+
     return 0;
 }
+
+    
+
 
 
